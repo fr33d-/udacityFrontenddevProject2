15d68dfb62968837d6478ebc058811148b1bd68e
Frederic Wollinger <fredericwollinger@gmail.com>
2018-03-20 19:08:31 +0100

    Works with limited restrictions

--- js/main.js
+++ js/main.js
@@ -9,14 +9,12 @@ var icons = [
 	'google'
 ];
 
+var cards = [];
+
 function fillGameField() {
 
 	var generatedHTML = '';
-
-	//Generate card field
 	var unshuffeledCards = icons.concat(icons);
-
-	var cards = [];
 
 	while (unshuffeledCards.length != 0) {
 		index = Math.floor(Math.random() * unshuffeledCards.length) ;
@@ -24,23 +22,91 @@ function fillGameField() {
 		unshuffeledCards.splice(index, 1);
 	}
 
-
 	cards.forEach(function(card, index) {
-	  generatedHTML += '<div class="field" id="field-1">\
-				<div class="card card-closed" id="card_'+index+'_closed"></div>\
-				<div class="card card-open closed" id="card_'+index+'_open"><i class="fab fa-'+card+'"></i></div>\
+	  generatedHTML += '<div class="field unfound" id="'+index+'">\
+				<div class="card card-closed"></div>\
+				<div class="card card-open closed"><i class="fab fa-'+card+'"></i></div>\
 			</div>';
 	});
 
 	$('.game').html(generatedHTML);
 }
 
+class Player {
+	constructor() {
+		this.turn = 1;
+		this.firstCard = 0;
+		this.secondCard = 0;
+		this.found = 0;
+	}
+
+	//Todo: wenn schon im zweiten zug die richtige karte gefunden wurde, gleich die leiste aktualisieren
+	//Todo: irgendwas anzeigen wenn man gewonnen hat.
+
+	playerTurn(cardID) {
+
+		if (this.turn == 1) {
+			$('#'+cardID).find('.card-open').toggleClass('closed');
+			$('#'+cardID).find('.card-closed').toggleClass('closed');
+			this.firstCard = cardID;
+			this.turn++;
+		} else if (this.turn == 2) {
+			$('#'+cardID).find('.card-open').toggleClass('closed');
+			$('#'+cardID).find('.card-closed').toggleClass('closed');
+			this.secondCard = cardID;
+			this.turn++;
+		} else {
+			this.turn = 1;
+
+			if (cards[this.firstCard] != cards[this.secondCard]) {
+				$('#'+this.firstCard).find('.card-open').toggleClass('closed');
+				$('#'+this.firstCard).find('.card-closed').toggleClass('closed');
+				$('#'+this.secondCard).find('.card-open').toggleClass('closed');
+				$('#'+this.secondCard).find('.card-closed').toggleClass('closed');
+			} else {
+				$('#'+this.firstCard).removeClass('unfound');
+				$('#'+this.secondCard).removeClass('unfound');
+
+				this.found++;
+				var list = '';
+
+				for (var i = this.found; i > 0; i-- ) {
+					list += '<li class="done"></li>';
+				}
+
+				for (var i = this.found; i < 8; i++ ) {
+					list += '<li class="open"></li>';
+				}
+
+				$('.progress ul').html(list);
+			}
+		}
+	}
+}
 
 $(function() {
 	fillGameField();
 
-	$('.field').on('click', function() {
-		$(this).find('.card-open').toggleClass('closed');
-		$(this).find('.card-closed').toggleClass('closed');
+	var player = new Player();
+
+	$('body').on('click', '.unfound', function() {
+		player.playerTurn( $(this).attr('id') );
 	});
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
