e4b785c284fd35920abc18e934f8c19538843945
Frederic Wollinger <fredericwollinger@gmail.com>
2018-03-20 16:27:21 +0100

    card flip effect and randomisazion, also fucked up the node_modules again

--- js/main.js
+++ js/main.js
@@ -1,3 +1,46 @@
-var icons [
-	'app-store',
-]
+var icons = [
+	'amazon',
+	'apple',
+	'angular',
+	'meetup',
+	'facebook',
+	'firefox',
+	'github',
+	'google'
+];
+
+function fillGameField() {
+
+	var generatedHTML = '';
+
+	//Generate card field
+	var unshuffeledCards = icons.concat(icons);
+
+	var cards = [];
+
+	while (unshuffeledCards.length != 0) {
+		index = Math.floor(Math.random() * unshuffeledCards.length) ;
+		cards.push(unshuffeledCards[index]);
+		unshuffeledCards.splice(index, 1);
+	}
+
+
+	cards.forEach(function(card, index) {
+	  generatedHTML += '<div class="field" id="field-1">\
+				<div class="card card-closed" id="card_'+index+'_closed"></div>\
+				<div class="card card-open closed" id="card_'+index+'_open"><i class="fab fa-'+card+'"></i></div>\
+			</div>';
+	});
+
+	$('.game').html(generatedHTML);
+}
+
+
+$(function() {
+	fillGameField();
+
+	$('.field').on('click', function() {
+		$(this).find('.card-open').toggleClass('closed');
+		$(this).find('.card-closed').toggleClass('closed');
+	});
+});
