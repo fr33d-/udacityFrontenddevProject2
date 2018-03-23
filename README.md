28bd278e093d6917267071bb05570c2e0087266f
Frederic Wollinger <fredericwollinger@gmail.com>
2018-03-23 11:11:58 +0100

    Review fixes

--- README.md
+++ README.md
@@ -1,15 +1,20 @@
 # Udacity frontend developer project Nr. 2
 
-Its a simple memory game. 
+Its a simple memory game.
 
 The animations work best in Google Chrome.
 
-Flip the cards and hope for the best, its like every memory game you've seen. If the cards match they will stay open and you got them, if not go for another try. 
+Flip the cards and hope for the best, its like every memory game you've seen. If the cards match they will stay open and you got them, if not go for another try.
 
-A indicator tells you how many items you found, how good you are and how much time you spend with the game. If you like it, tell others about it. 
+A indicator tells you how many items you found, how good you are and how much time you spend with the game. If you like it, tell others about it.
 
 ### Opening the game:
 You can run it from the index.html or as a developer runt the development server:
 1: run '> npm install'
 2: start the liveserver with '> gulp'
 
+### To the reviewer:
+1. I really disagree with the rule that it doesn't count as a move when you click twice on a card. It is now implemented like this but still i disagree.
+When you click on a card you can see what symbol it has. If you hide it again and your moves don't count up, you still know what is under the card.
+So you could easily cheat on the game.
+2. I can not reproduce the but with the stars, they work well here. Till 20 stars you got 3, till 30 2 and over 40 0 stars. Please tell me more about it if its not working for you.
--- js/main.js
+++ js/main.js
@@ -11,6 +11,9 @@ var icons = [
 ];
 var cards = [];
 var secondsPlayed = 0;
+var player = null;
+var stars = null;
+var timer = null;
 
 //Function to initial fill the game field.
 function fillGameField() {
@@ -22,30 +25,31 @@ function fillGameField() {
 	var unshuffeledCards = icons.concat(icons);
 
 	//Shuffle cards
-	while (unshuffeledCards.length != 0) {
-		index = Math.floor(Math.random() * unshuffeledCards.length) ;
+	while (unshuffeledCards.length !== 0) {
+		var index = Math.floor(Math.random() * unshuffeledCards.length) ;
 		cards.push(unshuffeledCards[index]);
 		unshuffeledCards.splice(index, 1);
 	}
 
 	//Fill the game html with every card
 	cards.forEach(function(card, index) {
-	  generatedHTML += '<div class="field unfound" id="'+index+'">\
-				<div class="card card-closed"></div>\
-				<div class="card card-open closed"><i class="fab fa-'+card+'"></i></div>\
-			</div>';
+	  generatedHTML += '<div class="field unfound" id="'+index+'">'+
+				'<div class="card card-closed"></div>'+
+				'<div class="card card-open closed"><i class="fab fa-'+card+'"></i></div>'+
+			'</div>';
 	});
 	$('.game').html(generatedHTML);
 }
 
 //Function the chef if the player has won
-function hasWon() {
+function hasWon(stars) {
 	//Display a modal if the player has won.
 	//Show the seconds he needed and give option to start the game again.
-	var restart = confirm('Congratulations! \n You won the game in '+secondsPlayed+' seconds. \n New game?');
-	if (restart == true) {
+	var restart = confirm('Congratulations! \n You won the game in '+secondsPlayed+' seconds with '+stars+' stars. \n New game?');
+	if (restart === true) {
 		fillGameField();
 	}
+	clearTimeout(timer);
 }
 
 
@@ -58,21 +62,35 @@ class Player {
 		this.found = 0;
 		this.firstTurnMade = false;
 		this.totalTurns = 0;
+		stars = 3;
+	}
+
+	movesCounter() {
+		//Counts the turns and removes the color of stars if he/she uses many turns
+
+		// To the reviewer how complained about $ as undefined: $ is the jQuery selector to address DOM elements.
+		// If this is not the best way to use it please tell me exactly why and how I can make it better.
+		// Please do not just write "undefined variables"! This is not helpful in any way.
+
+
+		this.totalTurns += 2;
+		if (this.totalTurns > 20 && this.totalTurns <= 30 ) {
+			$('#star-3').removeClass('active');
+			this.stars = 2;
+		} else if (this.totalTurns > 30 && this.totalTurns <= 40 ) {
+			$('#star-2').removeClass('active');
+			this.stars = 1;
+		} else if (this.totalTurns > 40 ) {
+			$('#star-1').removeClass('active');
+			this.stars = 0;
+		}
+
+		//count moves counter up
+		$('#moves').html(this.totalTurns+' moves');
 	}
 
 	//gets called every time the player clicks on a card
 	playerTurn(cardID) {
-
-		//Counts the turns and removes the color of stars if he/she uses many turns
-		this.totalTurns++;
-		if (this.totalTurns > 20 && this.totalTurns <= 30 ) {
-			$('#star-3').removeClass('active');
-		} else if (this.totalTurns > 30 && this.totalTurns <= 40 ) {
-			$('#star-2').removeClass('active');
-		} else if (this.totalTurns > 40 ) {
-			$('#star-1').removeClass('active');
-		}
-		$('#moves').html(this.totalTurns+' moves');
 
 		//If its the first card you just flip one card
 		if (this.turn == 1) {
@@ -84,7 +102,7 @@ class Player {
 
 			//Check if it was the first turn, if so start a timer.
 			if (!this.firstTurnMade) {
-				var timer = window.setInterval(function () {
+				timer = window.setInterval(function () {
 			    	secondsPlayed++;
 					$('#timer').html(secondsPlayed+' s');
 				}, 1000);
@@ -107,7 +125,7 @@ class Player {
 				for (var i = this.found; i > 0; i-- ) {
 					list += '<li class="done"></li>';
 				}
-				for (var i = this.found; i < 8; i++ ) {
+				for (var j = this.found; j < 8; j++ ) {
 					list += '<li class="open"></li>';
 				}
 				$('.progress-indicator ul').html(list);
@@ -119,10 +137,11 @@ class Player {
 
 				//And check if the player has won.
 				if (this.found == 8) {
-					setTimeout(function(){
-						hasWon();
+					timer = setTimeout(function(){
+						hasWon(stars);
 					}, 1000);
 				}
+				this.movesCounter()
 
 			} else {
 				//Cards don't match. Turn both cards after the first animation has finished
@@ -134,6 +153,8 @@ class Player {
 					$('#'+secondCard).find('.card-open').toggleClass('closed');
 					$('#'+secondCard).find('.card-closed').toggleClass('closed');
 				}, 1000);
+
+				this.movesCounter()
 			}
 
 		}
